5f84fcdac8ebeeab23b8ce8f18e570e802e14a88
Jimena <jimenaa971@gmail.com>
2022-07-15 21:11:19 +0200

    style(App): add basic format
    Add style to the title text, the background and the status bar.

--- App.tsx
+++ App.tsx
@@ -3,9 +3,7 @@ import {
   SafeAreaView,
   StatusBar,
   StyleSheet,
-  useColorScheme,
 } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
 import { Text } from 'react-native-paper';
 
 import { AllCardItems, FilterBar } from './containers';
@@ -19,18 +17,12 @@ const client = new ApolloClient({
 
 const App = () => {
   const [categorySelected, setCategorySelected] = React.useState<string>('');
-  
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   return (
     <ApolloProvider client={client}>
-      <SafeAreaView style={backgroundStyle}>
-        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-          <Text>Learn</Text>
+      <SafeAreaView style={styles.screen}>
+        <StatusBar barStyle={'light-content'} backgroundColor={'#3F0259'}/>
+          <Text style={styles.title}>Learn</Text>
           <FilterBar categorySelected={categorySelected} setCategorySelected={setCategorySelected} />
           <AllCardItems category={categorySelected} />
       </SafeAreaView>
@@ -39,22 +31,17 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  title: { 
+    fontSize: 30,
+    fontWeight:'bold',
+    alignSelf: 'center',
+    marginBottom: 5,
+    marginTop: 20,
+    color: 'white'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  screen: {
+    backgroundColor: '#3F0259'
+  }
 });
 
 export default App;
